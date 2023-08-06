e1a6b94ebfb45be42f15601d48268a7077653a1c
RynerNO <ryner.no@gmail.com>
2023-08-06 21:33:52 +0200

    mobile version of nav bar and header

--- src/js/main.js
+++ src/js/main.js
@@ -11,4 +11,17 @@ initBootstrap({
   toasts: true,
 });
 
+const navMenu = document.querySelector('.nav-menu');
+
+const mobileMenuBtn = document.querySelector('#mobileMenu')
+let navMenuTimeoutId = null
+mobileMenuBtn.addEventListener('click', () => {
+  if(navMenuTimeoutId !== null) clearTimeout(navMenuTimeoutId)
+  navMenu.classList.toggle('nav-menu_visible')
+  if(navMenu.classList.contains('nav-menu_visible')) {
+    navMenuTimeoutId = setTimeout(() => {
+      navMenu.style.height = "auto"
+    }, 300)
+  } else navMenu.style.height = "0px"
+})
 
