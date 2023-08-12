cc3762c1b6379a866cdd18d5de0f8a027b239350
RynerNO <ryner.no@gmail.com>
2023-08-12 18:09:13 +0200

    main page fixes

--- src/js/main.js
+++ src/js/main.js
@@ -67,7 +67,21 @@ const popularSwiper = new Swiper('#popular-slider', {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    800: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+  }
 });
 
 
